d4ddb717821a9afedd1a459a98411b6009adb3fe
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-09-28 12:55:14 +0300

    update Terraform resources

--- Terraform/resourse.tf
+++ Terraform/resourse.tf
@@ -4,8 +4,8 @@ resource "null_resource" "out" {
   }
   provisioner "local-exec" {
     command =<<EOF
-      ../Script/inventory.sh ${module.EC2.ec2["public1"].public_ip} ${module.EC2.ec2["public2"].public_ip} ${module.EC2.ec2["private1"].private_ip} ${module.EC2.ec2["private2"].private_ip} ${module.Load_Balancer.load_balancers["private"].dns_name}
-      ../Script/ssh_config.sh ${module.EC2.ec2["public1"].public_ip} ${module.EC2.ec2["private1"].private_ip} ${module.EC2.ec2["private2"].private_ip}
+      ../Scripts/inventory.sh ${module.EC2.ec2["public1"].public_ip} ${module.EC2.ec2["public2"].public_ip} ${module.EC2.ec2["private1"].private_ip} ${module.EC2.ec2["private2"].private_ip} ${module.Load_Balancer.load_balancers["private"].dns_name}
+      ../Scripts/ssh_config.sh ${module.EC2.ec2["public1"].public_ip} ${module.EC2.ec2["private1"].private_ip} ${module.EC2.ec2["private2"].private_ip}
     EOF
   }
 
