8e794bd0246b95249426647efa7de4a490987d8f
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-09-28 12:59:41 +0300

    update Terraform resources

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -12,3 +12,8 @@ output "natg" {
 output "my_public_ip" {
   value = data.http.myip.response_body
 }
+
+output "load_balancer_dns" {
+  value = module.Load_Balancer.load_balancers["public"].dns_name
+  
+}
