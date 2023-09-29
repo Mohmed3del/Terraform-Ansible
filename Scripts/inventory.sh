6d9f56fa95ef251675a8faaf56178998b8602f6a
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-09-30 00:46:36 +0300

    chmod scripts to 750

--- Scripts/inventory.sh
+++ Scripts/inventory.sh
@@ -7,7 +7,7 @@ bastion2 ansible_host=$2
 private1 ansible_host=$3
 private2 ansible_host=$4
 
-[private_ec2:vars]
+[bastion:vars]
 application_dns=$5
 
 EOF
--- Scripts/ssh_config.sh
+++ Scripts/ssh_config.sh
@@ -2,17 +2,20 @@ cat <<EOF > $HOME/.ssh/config
 host bastion
    HostName $1
    User ubuntu
+   IdentityFile $HOME/projects/Terraform_Ansible/keys/DevOps.pem
    StrictHostKeyChecking=no
 
 host private1
    HostName  $2
    user  ubuntu
+   IdentityFile $HOME/projects/Terraform_Ansible/keys/DevOps.pem
    ProxyCommand ssh bastion -W %h:%p
    StrictHostKeyChecking=no
 
 host private2
    HostName  $3
    user  ubuntu
+   IdentityFile $HOME/projects/Terraform_Ansible/keys/DevOps.pem
    ProxyCommand ssh bastion -W %h:%p
    StrictHostKeyChecking=no
 EOF
